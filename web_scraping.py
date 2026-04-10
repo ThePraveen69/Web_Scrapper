05014225a70880d48735dae13c12c8c6e6cce6ae
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-10 18:43:32 +0530

    Modify web scraping to print title tag
    Updated the web scraping script to extract and print the title tag.

--- web_scraping.py
+++ web_scraping.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
-
 with open("index.html","r") as f:
     doc = BeautifulSoup(f,"html.parser")
-print(doc.prettify())
+#print(doc.prettify())
+tag = doc.title
+print(tag)
+print(tag.string)
