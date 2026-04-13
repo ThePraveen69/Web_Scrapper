14a5e37f008186ebd45eb4cc9ea2c054d46d86c3
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 07:20:43 +0530

    Enhance web scraping to prettify HTML output
    Added parsing of HTML document using BeautifulSoup.

--- web_scraping.py
+++ web_scraping.py
@@ -2,4 +2,5 @@ from bs4 import BeautifulSoup
 import requests
 url ="https://www.newegg.com/tools/combo-builder/1740?cm_sp=homepage-pers-home%20dynamiccombo_pccomponent-amd-takeover"
 result = requests.get(url)
-print(result.text)
+doc = BeautifulSoup(result.text,"html.parser")
+print(doc.prettify())
