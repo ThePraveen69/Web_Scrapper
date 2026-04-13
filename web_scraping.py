755c59aa0e5eb2ce046885c605ce0fa7711df506
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 18:17:41 +0530

    Update option tag attributes in web_scraping.py
    Modify the selected option and set its value in the web scraping script.

--- web_scraping.py
+++ web_scraping.py
@@ -2,6 +2,6 @@ from bs4 import BeautifulSoup
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
 tag = doc.find("option")
-tags = doc.find_all("option")
+tag["selected"] = "false"
+tag['value'] = "praveen"
 print(tag)
-print(tags)
