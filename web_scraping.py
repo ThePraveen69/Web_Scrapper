004afbf2f26eb4a42a191165434aa2489f08314f
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 07:32:54 +0530

    Modify web scraping URL and extract prices
    Updated URL to scrape a specific graphics card page and added functionality to extract prices.

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
-url ="https://www.newegg.com/tools/combo-builder/1740?cm_sp=homepage-pers-home%20dynamiccombo_pccomponent-amd-takeover"
+url ="https://www.newegg.com/gigabyte-gv-n3080vision-oc-10gd-geforce-rtx-3080-10gb-graphics-card-windforce-3x/p/N82E16814932460"
 result = requests.get(url)
 doc = BeautifulSoup(result.text,"html.parser")
-print(doc.prettify())
+prices = doc.find_all(text ="$")
+print(prices)
