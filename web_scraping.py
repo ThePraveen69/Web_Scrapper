d73cd18b9947a8cf151e4e14c807ba3dd2c7f581
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 07:39:17 +0530

    Print parent element of the first price found
    Added functionality to print the parent element of the first price found.

--- web_scraping.py
+++ web_scraping.py
@@ -3,5 +3,6 @@ import requests
 url ="https://www.newegg.com/gigabyte-gv-n3080vision-oc-10gd-geforce-rtx-3080-10gb-graphics-card-windforce-3x/p/N82E16814932460"
 result = requests.get(url)
 doc = BeautifulSoup(result.text,"html.parser")
-prices = doc.find_all(text ="$")
-print(prices)
+prices = doc.find_all(text="$")
+parent = prices[0].parent
+print(parent)
