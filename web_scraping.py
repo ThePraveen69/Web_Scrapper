7870c818ca1fad75f7b113d23cdff2c7c327ecb1
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 18:14:41 +0530

    Refactor web scraping to use local HTML file
    Removed web scraping from Newegg and added local HTML file parsing.

--- web_scraping.py
+++ web_scraping.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
-import requests
-url ="https://www.newegg.com/gigabyte-gv-n3080vision-oc-10gd-geforce-rtx-3080-10gb-graphics-card-windforce-3x/p/N82E16814932460"
-result = requests.get(url)
-doc = BeautifulSoup(result.text,"html.parser")
-prices = doc.find_all(text="$")
-parent = prices[0].parent
-print(parent)
+with open("index2.htm","r") as f:
+    doc = BeautifulSoup(f,"html.parser")
+tag = doc.find("option")
+tags = doc.find_all("option")
+print(tag)
+print(tags)
