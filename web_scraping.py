3109eb2764be02be6f3b945e7ba17e87ea5b8a05
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-13 18:22:22 +0530

    Print attributes of the selected tag in web scraping
    Updated the code to print the attributes of the selected tag instead of modifying them.

--- web_scraping.py
+++ web_scraping.py
@@ -2,6 +2,4 @@ from bs4 import BeautifulSoup
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
 tag = doc.find("option")
-tag["selected"] = "false"
-tag['value'] = "praveen"
-print(tag)
+print(tag.attrs)
