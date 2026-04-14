543511a3ec2abdaf4917118a622541ccc0300c46
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-14 19:28:28 +0530

    Strip whitespace from printed tags in web scraping
    Added a loop to strip whitespace from each printed tag.

--- web_scraping.py
+++ web_scraping.py
@@ -3,7 +3,6 @@ import re
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
 tags = doc.find_all(text = re.compile("\$.*"))
-
-
 print(tags)
-
+for tag in tags:
+    print(tag.strip())
