b239f0cfa5b903e380be7a6241750bad8ec42d11
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-14 18:45:21 +0530

    Update web scraping to find multiple tags

--- web_scraping.py
+++ web_scraping.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
-tag = doc.find("option")
-print(tag.attrs)
+tags = doc.find_all(["p",'option','div'])
+print(tags)
