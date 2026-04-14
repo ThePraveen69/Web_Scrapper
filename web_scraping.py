836f1403550df065e0a3c4fda5157d041672c693
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-14 19:25:35 +0530

    Refactor web scraping to extract dollar amounts with regex
    Updated the web scraping logic to utilize regex for extracting dollar amounts directly from the text content of HTML elements.

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
+import re
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
-tags = doc.find_all(["p",'option','div'])
-tag1 = doc.find_all(class_="btn-item")
-print(tag1)
+tags = doc.find_all(text = re.compile("\$.*"))
+
+
+print(tags)
+
