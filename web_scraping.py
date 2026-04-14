387bf5fcb03860f09b730f410490a4a8ffa680b9
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-14 19:00:14 +0530

    Add functionality to find tags with class 'btn-item'

--- web_scraping.py
+++ web_scraping.py
@@ -2,4 +2,5 @@ from bs4 import BeautifulSoup
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
 tags = doc.find_all(["p",'option','div'])
-print(tags)
+tag1 = doc.find_all(class_="btn-item")
+print(tag1)
