c4ac2942d446862b1b70ccd68f0d1f1f75971247
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-14 19:33:58 +0530

    Limit tag search to one result
    Limit the number of tags found to 1.

--- web_scraping.py
+++ web_scraping.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import re
 with open("index2.htm","r") as f:
     doc = BeautifulSoup(f,"html.parser")
-tags = doc.find_all(text = re.compile("\$.*"))
+tags = doc.find_all(text = re.compile("\$.*"),limit = 1)
 print(tags)
 for tag in tags:
     print(tag.strip())
