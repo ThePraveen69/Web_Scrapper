9063410f66d8f371e5c203aafa864fd9728430d5
ThePraveen69 <praveenkumaryerrapilli69@gmail.com>
2026-04-15 17:15:07 +0530

    Modify print statements to access parent elements

--- web_scraping.py
+++ web_scraping.py
@@ -5,7 +5,5 @@ result = requests.get(url)
 doc = BeautifulSoup(result.text,"html.parser")
 tbody = doc.tbody
 trows = tbody.contents
-print(trows[0])
-print(trows[0].next_sibling)
-print(trows[1].previous_sibling)
-print(list(trows[0].next_siblings))
+print(trows[0].parent)
+print(trows[0].parent.name)
